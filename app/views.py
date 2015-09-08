4804c3cd50150a4c557af3e2cf0458b0ded263ff
Margie Roswell <mroswell@gmail.com>
2015-09-08 09:12:41 -0500

    Add my contact info

--- app/views.py
+++ app/views.py
@@ -8,10 +8,10 @@ from django.template import RequestContext
 from datetime import datetime
 
 YOUR_INFO = {
-    'name' : 'Your name',
-    'bio' : 'What\'s your deal? What do you do?',
+    'name' : 'Office Chef',
+    'bio' : 'A System for Deliciously Fast and Energizing Meals at Work',
     'email' : '', # Leave blank if you'd prefer not to share your email with other conference attendees
-    'twitter_username' : 'tweettweet', # No @ symbol, just the handle.
+    'twitter_username' : 'office_chef', # No @ symbol, just the handle.
     'github_username' : "fetchpush", 
     'headshot_url' : '', # Link to your GitHub, Twitter, or Gravatar profile image.
 }
